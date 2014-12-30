9750f411e6fb37aa82e46dbb5ffb56477be90968
Jonathan Irvin Gunawan <jonathan.irvin@yahoo.com>
2014-12-30 13:33:45 +0700

    Finished problem.js

--- js/problem.js
+++ js/problem.js
@@ -8,13 +8,33 @@ Problem.prototype.addSubmission = function(submission) {
 }
 
 Problem.prototype.getFirstSolve = function() {
+  for (var i = 0; i < this.submissions.length; ++i) {
+    if (this.submissions[i].verdict == yesSubmission) {
+      return this.submissions[i].team;
+    }
+  }
   return null;
 }
 
 Problem.prototype.getNumberAttempts = function() {
-  return 0;
+  return this.submissions.length;
 }
 
 Problem.prototype.getNumberAccepted = function() {
-  return 0;
+  var numberOfAccepted = 0;
+  var solvedBy = {};
+  for (var i = 0; i < this.submissions.length; ++i) {
+    var submission = this.submissions[i];
+    if (!submission.team in solvedBy) {
+      solvedBy[submission.team] = false;
+    }
+    if (solvedBy[submission.team]) {
+      continue;
+    }
+    if (submission.verdict == yesSubmission) {
+      ++numberOfAccepted;
+      solvedBy[submission.team] = true;
+    }
+  }
+  return numberOfAccepted;
 }
