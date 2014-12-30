93f0a3188291108a2fb36f47e8cd00aa140543be
Jonathan Irvin Gunawan <jonathan.irvin@yahoo.com>
2014-12-30 13:02:08 +0700

    Add team.js

--- js/constant.js
+++ js/constant.js
@@ -1,3 +1,9 @@
 var runListLocation = "RunList";
 var problemListLocation = "ProblemList";
 var teamListLocation = "TeamList";
+
+var yesSubmission = "YES";
+var noSubmission = "NO";
+var pendingSubmission = "PENDING";
+
+var noSubmissionPenalty = 20;
--- js/team.js
+++ js/team.js
@@ -4,17 +4,49 @@ function Team() {
 }
 
 Team.prototype.getPenalty = function() {
-  return 0;
+  var totalPenalty = 0;
+  var penalty = {};
+  var isSolved = {};
+  for (var i = 0; i < this.submissions.length; ++i) {
+    submission = this.submissions[i];
+    if ((!submission.problem in penalty) || (!submission.problem in isSolved)) {
+      penalty[submission.problem] = 0;
+      isSolved[submission.problem] = false;
+    }
+    if (isSolved[submission.problem]) {
+      continue;
+    }
+    if (submission.verdict == noSubmission) {
+      penalty[submission.problem] += noSubmissionPenalty;
+    } else if (submission.verdict == yesSubmission) {
+      totalPenalty += penalty[submission.problem] + submission.time;
+      isSolved[submission.problem] = true;
+    }
+  }
+  return totalPenalty;
 }
 
 Team.prototype.getNumberSolved = function() {
-  return 0;
+  var totalSolved = 0;
+  var isSolved = {};
+  for (var i = 0; i < this.submissions.length; ++i) {
+    submission = this.submissions[i];
+    if (!submission.problem in isSolved) {
+      isSolved[submission.problem] = false;
+    }
+    if (isSolved[submission.problem]) {
+      continue;
+    }
+    if (submission.verdict == yesSubmission) {
+      ++totalSolved;
+      isSolved[submission.problem] = true;
+    }
+  }
+  return totalSolved;
 }
 
 Team.prototype.addSubmission = function(submission) {
   this.submissions.push(submission);
 }
 
-Team.prototype.getFirstACSubmission = function(problemName) {
-  return null;
-}
+
